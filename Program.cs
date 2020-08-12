4dcb2da593a7a7a6df7a8a89b5f968ec07fb7bd0
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2020-08-12 13:51:25 +1000

    adding some stuff

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ namespace exceptions_calsswork
         {
         DateTime FirstDate = new DateTime(2020, 8, 03, 14, 30, 0);
         DateTime SecondDate = new DateTime(2020, 2, 23, 8, 20, 0);
+        TimeSpan stayed12 = new TimeSpan(0,0,12,0);
+        TimeSpan stayed15 = new TimeSpan(0,0,15,0);
+        TimeSpan stayed300 = new TimeSpan(0,0,300,0);
 
         Room r1 = new Room("TD224");
         Room r2 = new Room("AGSE111");
@@ -30,49 +33,12 @@ namespace exceptions_calsswork
                     p1.setDate(FirstDate);
                 }
             }
-            if (userInput == "Jane Black")
-            {
-                p2.addName(userInput);
-                System.Console.WriteLine("Which room are you looking for TD224 or AGSE111 ?");
-                string userRoomInput = Console.ReadLine();
-                if (userRoomInput == "TD224")
-                {
-                    r1.enterRoom(p2);
-                    p1.setDate(SecondDate);
-                }
-                
-            }
 
-        } catch (FormatException ex) {
-            System.Console.WriteLine($"Your input was not able to be understood :( Closing Program - {ex.Message}");
-        } catch (NullReferenceException ex) {
-            System.Console.WriteLine($"Object doesn't exist :( Closing Program - {ex.Message}");
         } catch (Exception ex) {
-            System.Console.WriteLine($"Something went wrong :( Closing Program - {ex.Message}");
-        } finally {
-            System.Console.WriteLine("!!!Finally block!!!");
+            System.Console.WriteLine($"Your input was not able to be understood or something else you did :( Closing Program - {ex.Message}");
         }
-
-        System.Console.WriteLine("Are you both ready to leave the room?");
-        
-        try {
-        
-        
-            
-        } catch (FormatException ex) {
-            System.Console.WriteLine($"Input can't be converted to an integer :( Closing Program - {ex.Message}");
-            //System.Environment.Exit(1);
-        } catch (NullReferenceException ex) {
-            System.Console.WriteLine($"Object doesn't exist :( Closing Program - {ex.Message}");
-            //System.Environment.Exit(1);
-        } catch (Exception ex) {
-            System.Console.WriteLine($"Something went wrong :( Closing Program - {ex.Message}");
-            //System.Environment.Exit(1);
-        } finally {
-            
-            System.Console.WriteLine("!!!Finally block!!!");
-        }
-
+           
+       
 
         }
     }
