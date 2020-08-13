b8747af3601b3739f6397657b7e17519d9cc330a
Ben Gardiner <bennygards@gmail.com>
2020-08-13 23:26:41 +1000

    need to add the times stayed in a function

--- Program.cs
+++ Program.cs
@@ -8,13 +8,19 @@ namespace exceptions_calsswork
         {
         DateTime FirstDate = new DateTime(2020, 8, 03, 14, 30, 0);
         DateTime SecondDate = new DateTime(2020, 2, 23, 8, 20, 0);
+
+        TimeSpan stayed12 = new TimeSpan(0,0,12,0);
+        TimeSpan stayed15 = new TimeSpan(0,0,15,0);
+        TimeSpan stayed300 = new TimeSpan(0,0,300,0);
+
     
         Room r1 = new Room("TD224");
         Room r2 = new Room("AGSE111");
         Person p1 = new Person();
         Person p2 = new Person();
 
-
+        //need to break this into a function
+        
         System.Console.WriteLine("Before you go into this room person 1 tell me your name: John Doe or Jane Black ?");
         string userInput = Console.ReadLine();
         identityAssist(userInput);
@@ -33,13 +39,18 @@ namespace exceptions_calsswork
             p1.setDate(FirstDate);
         }       
            
+        //need to break these int functions
+
         System.Console.WriteLine($"{p2.Name} which room wil you enter TD224 or AGSE111?");
         string user2RoomInput = Console.ReadLine();
-        
+        System.Console.WriteLine($"{p2.Name} How long will you stay 12, 15, or 300 minutes");
+        string user2timeInput;
+
         if (user2RoomInput == "TD224" && r1.isRoomEmpty())
         {
             r1.enterRoom(p2);
             p2.setDate(SecondDate);
+            ///need to amke a functoin ehre that sets the time stayed fir users
         }
         else if (user2RoomInput == "AGSE111" && r2.isRoomEmpty())
         {
@@ -47,6 +58,7 @@ namespace exceptions_calsswork
             p2.setDate(SecondDate);
         }      
         
+
 
 
 
